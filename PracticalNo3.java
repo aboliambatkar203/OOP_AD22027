fbe0e74ff1109affee2205c9e97315c08ae01ec1
aboliambatkar <aboliambatkar203@gmail.com>
2024-02-23 16:35:59 -0200

    Completed Succesfully

---
+++ PracticalNo3.java
@@ -0,0 +1,52 @@
+class Volume
+
+{
+
+   public Double calculateVolume (Double sideLength)
+
+   {
+     return sidelength sidelength sidelength;
+
+    }
+
+     public Double calculateVolume (Double radius, Double height)
+
+{
+
+}
+
+return Math.PI radius radius height;
+
+public Double calculateVolume (Double length, Double width, Double height)
+
+{
+
+}
+
+return length width height;
+
+public class PracticalNo3
+
+{
+
+public static void main(String[] args)
+
+{
+
+Volume Volume new Volume();
+
+Double cubeVolume = Volume.calculateVolume(5.0);
+
+System.out.println("Volume of cube with side length 5:---->" + cubeVolume);
+
+Double cylinder Volume Volume.calculateVolume (3.0, 5.0);
+
+System.out.println("Volume of cylinder with radius 3 and height 5:---->" + cylinderVolume);
+
+Double boxVolume Volume.calculateVolume(4.0, 6.0, 8.0);
+
+System.out.println("Volume of box with length 4, width 6, and height 8:---->" + boxVolume);
+
+}
+
+}
