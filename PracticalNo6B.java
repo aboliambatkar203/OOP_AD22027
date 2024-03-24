ffcb665aa53ebbb9db77b07a8b3b7caedd04f7b7
aboliambatkar <aboliambatkar203@gmail.com>
2024-03-24 12:08:06 -0200

    PracticalNo 6B completed

---
+++ PracticalNo6B.java
@@ -0,0 +1,7 @@
+import learnjava.PracticalNo6A;
+public class PracticalNo6B {
+public static void main(String[] args) {
+PracticalNo6A firstObject = new PracticalNo6A();
+firstObject.display();
+}
+}
